9a78f0f3156697ae0c04921ca7427bf8f9af1982
Ilya Kultinov <kultinov.i@gmail.com>
2023-10-30 22:37:00 +0300

    update views

--- orders/app/models.py
+++ orders/app/models.py
@@ -251,43 +251,43 @@ class OrderItem(models.Model):
         constraints = [
             models.UniqueConstraint(fields=['order_id', 'product_info'], name='unique_order_item'),
         ]
-# Нет того что ниже
 
-# class ConfirmEmailToken(models.Model):
-#     class Meta:
-#         verbose_name = 'Токен подтверждения Email'
-#         verbose_name_plural = 'Токены подтверждения Email'
 
-#     @staticmethod
-#     def generate_key():
-#         """ generates a pseudo random code using os.urandom and binascii.hexlify """
-#         return get_token_generator().generate_token()
+class ConfirmEmailToken(models.Model):
+    class Meta:
+        verbose_name = 'Токен подтверждения Email'
+        verbose_name_plural = 'Токены подтверждения Email'
 
-#     user = models.ForeignKey(
-#         User,
-#         related_name='confirm_email_tokens',
-#         on_delete=models.CASCADE,
-#         verbose_name=_("The User which is associated to this password reset token")
-#     )
+    @staticmethod
+    def generate_key():
+        """ generates a pseudo random code using os.urandom and binascii.hexlify """
+        return get_token_generator().generate_token()
 
-#     created_at = models.DateTimeField(
-#         auto_now_add=True,
-#         verbose_name=_("When was this token generated")
-#     )
+    user = models.ForeignKey(
+        User,
+        related_name='confirm_email_tokens',
+        on_delete=models.CASCADE,
+        verbose_name=_("The User which is associated to this password reset token")
+    )
 
-#     # Key field, though it is not the primary key of the model
-#     key = models.CharField(
-#         _("Key"),
-#         max_length=64,
-#         db_index=True,
-#         unique=True
-#     )
+    created_at = models.DateTimeField(
+        auto_now_add=True,
+        verbose_name=_("When was this token generated")
+    )
 
-#     def save(self, *args, **kwargs):
-#         if not self.key:
-#             self.key = self.generate_key()
-#         return super(ConfirmEmailToken, self).save(*args, **kwargs)
+    # Key field, though it is not the primary key of the model
+    key = models.CharField(
+        _("Key"),
+        max_length=64,
+        db_index=True,
+        unique=True
+    )
 
-#     def __str__(self):
-#         return "Password reset token for user {user}".format(user=self.user)
+    def save(self, *args, **kwargs):
+        if not self.key:
+            self.key = self.generate_key()
+        return super(ConfirmEmailToken, self).save(*args, **kwargs)
+
+    def __str__(self):
+        return "Password reset token for user {user}".format(user=self.user)
 
